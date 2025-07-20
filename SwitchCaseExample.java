b952cb973f7f38fc6fc9f0172977c16c2237b453
Cazaks <calebezak@gmail.com>
2025-07-20 05:22:59 +0100

    Addint default case

--- SwitchCaseExample.java
+++ SwitchCaseExample.java
@@ -3,7 +3,7 @@ public class SwitchCaseExample {
 	public static void main(String[] args){
 
 	Scanner scan = new Scanner(System.in);
-	System.out.println("Eneter a number from 1-3");
+	System.out.println("Eneter a number from 1-4");
 	int number = scan.nextInt();
 
 	switch (number) {
@@ -19,6 +19,9 @@ public class SwitchCaseExample {
     	case 4:
         	System.out.println("Four");
         	break;
+	default:
+		System.out.println("Invalid input. Kindly enter a number betwent 1 to 4");
+
 		}
 	}
 }
