ad42f8726aaf5fde644e5149f3d82f354fced376
Cazaks <calebezak@gmail.com>
2025-07-20 05:18:28 +0100

    Adding break to the cases

--- SwitchCaseExample.java
+++ SwitchCaseExample.java
@@ -9,16 +9,16 @@ public class SwitchCaseExample {
 	switch (number) {
    	 case 1:
         	System.out.println("One");
-        	
+        	break;
     	case 2:
         	System.out.println("Two");
-        	
+        	break;
     	case 3:
         	System.out.println("Three");
-        	
+        	break;
     	case 4:
         	System.out.println("Four");
-        	
+        	break;
 		}
 	}
 }
